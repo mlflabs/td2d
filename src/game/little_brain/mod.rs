d1d3d12a2bfb049e545c2aed3ec64835bf2336e5
Michal Zak <mlflabs@hotmail.com>
2023-12-07 22:48:04 +0800

    testing const values

--- src/game/little_brain/mod.rs
+++ src/game/little_brain/mod.rs
@@ -10,11 +10,21 @@ pub mod scorers;
 use bevy_xpbd_2d::parry::utils::hashmap;
 pub use scorers::*;
 
+use super::{Player, MainCamera};
+
 pub struct LittleBrainPlugin;
 
 impl Plugin for LittleBrainPlugin {
     fn build(&self, app: &mut App) {
 
+        let p = Point::new(2.,2.);
+
+        app
+            .add_systems(Update, (
+                camera_move,
+                test_system,
+                //p.test_system
+            ));
 
 
         let mut services: HashMap<String, Box<dyn MyTrait>> = HashMap::new();
@@ -25,13 +35,78 @@ impl Plugin for LittleBrainPlugin {
         //     ("rust".to_string(), Box::new(Foo))
         // ]);
 
-        let mut s: HashMap<String, dyn Component> = HashMap::new();
-        s.insert("rust".to_string(), Test2);
-        s.insert("rust2".to_string(), Test);
+        // let mut s: HashMap<String, dyn Component> = HashMap::new();
+        // s.insert("rust".to_string(), Test2);
+        // s.insert("rust2".to_string(), Test);
     }
 }
 
+
+const X:f32 = 10.0;
+
+pub fn test_system(
+    player_query: Query<&GlobalTransform, With<Player>>,
+){
+    print!("T1: {}", X);
+}
+
+
+pub fn camera_move(
+    player_query: Query<&GlobalTransform, With<Player>>,
+    mut camera_query: Query<(&mut Transform, &mut OrthographicProjection), With<MainCamera>>,    
+){
+    print!("T1: {}", X);
+}
+
+
 ///// tag, 
+pub struct Point {
+    pub x: f32,
+    pub y: f32,
+}
+
+impl Point {
+    fn origin() -> Point {
+        Point { x: 0.0, y: 0.0 }
+    }
+
+    // Another associated function, taking two arguments:
+    fn new(x: f32, y: f32) -> Point {
+        Point { x: x, y: y }
+    }
+
+    fn print(&self) {
+        println!("Testing Point {}", self.x);
+    }
+
+
+    pub fn test_system(
+        player_query: Query<&GlobalTransform, With<Player>>,
+    ){
+        print!("T1: {}", X);
+    }
+
+    pub fn test_system2(
+        &self,
+        cmd: &mut Commands,
+    ){
+        print!("T1: {}", self.x.clone());
+    }
+}
+
+
+
+// pub struct TestTest5 {
+//     pub t1: f32,
+// }
+
+// impl TestTest5 {
+//     pub fn wander_scorer_system(
+//         cmd: &mut Commands,
+//     ){
+//         print!("T1: {}", t1);
+//     }
+// }
 
 
 
